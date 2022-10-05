890725f910c8d14a47774775699ba3139b936394
mycooldj.eth <58099602+MyCool-DJ@users.noreply.github.com>
2022-10-05 11:34:28 +0000

    Notes added and updated a variable name on line 5

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-print("Hi I am your AI teller")
+#day 10 if my first 100days of code challenge 
+print("Hi, I am your AI teller")
 bill = int(input("What is your total bill? "))
 tip = int(input("Enter the amount you want to tip, 15%, 18%, or 20%? "))
-spliting = int(input("And how many people are we splitting that between? "))
+splitting = int(input("And how many people are we splitting that between? "))
 tip_amount = (tip / 100) * bill
 grand_total = tip_amount + bill
 print("Your grand total is,", grand_total)
