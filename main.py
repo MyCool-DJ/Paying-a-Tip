69b6c3082d5f30f23754de9038b8860efe9bc395
mycooldj.eth <58099602+MyCool-DJ@users.noreply.github.com>
2022-10-05 11:26:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Hi I am your AI teller")
+bill = int(input("What is your total bill? "))
+tip = int(input("Enter the amount you want to tip, 15%, 18%, or 20%? "))
+spliting = int(input("And how many people are we splitting that between? "))
+tip_amount = (tip / 100) * bill
+grand_total = tip_amount + bill
+print("Your grand total is,", grand_total)
+print("Thank you")
